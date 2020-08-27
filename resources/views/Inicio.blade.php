7016b60b26b4f6b5de5b2fbfca1a73651eefc814
xxskaxx1 <sergiordb1@gmail.com>
2020-08-27 17:56:10 -0500

    Presentación

--- resources/views/Inicio.blade.php
+++ resources/views/Inicio.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Tienda TUL</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -215,6 +215,7 @@
                     $("#carro_confirma").show();
                     $("#tabla_carrito tbody").empty();
                     $("#Checkout").hide();
+                    $(":button").hide();
                 }
             }
         });
